6c2fe68aeb0023a48d357892f8dc642d42e7b0c9
hungnq-devObs <hp.nguyenquochung@gmail.com>
2023-01-16 12:23:56 +0700

    test

---
+++ lambda-cd/lambda-cd.py
@@ -0,0 +1,13 @@
+# function.py:
+import json
+import psycopg2
+import psycopg2.extras
+import os
+ # Comment test continuous deployment 66
+
+def lambda_handler(event, context):
+    db_name = os.environ['DB_NAME']
+    db_user = os.environ['DB_USER']
+    db_host = os.environ['DB_HOST']
+    db_port = os.environ['DB_PORT']
+    db_pass = os.environ['DB_PASS']
